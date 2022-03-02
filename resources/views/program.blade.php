384c795c2789c2e678ad434ca9ace29a6d4d89dc
judhacdc <jdxxaa@gmail.com>
2022-03-02 10:21:27 +0700

    update

--- resources/views/program.blade.php
+++
@@ -1,1 +0,0 @@
-Ini adalaha halamn program
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\ProgramController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', function () {
     return view('welcome');
 });

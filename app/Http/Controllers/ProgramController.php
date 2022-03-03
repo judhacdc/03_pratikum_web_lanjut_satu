5ddb8ecab38a6a9be888d0d7c309656fd5f3ae51
judhacdc <jdxxaa@gmail.com>
2022-03-03 07:40:36 +0700

    update

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -7,8 +7,14 @@ use Illuminate\Http\Request;
 class ProgramController extends Controller
 {
     public function program(){
-        return view('program');
+        return view('program.index');
     }
 
+    public function program1(){
+        return view('program.program1');
+    }
 
+    public function program2(){
+        return view('program.program2');
+    }
 }
